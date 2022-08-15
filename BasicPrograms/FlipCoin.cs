d1e0308b9c1bf93b04dbee626213f11dce26622a
tejaswinibute <tejaswinibute31@gmail.com>
2022-08-15 17:51:52 +0530

    [Tejaswini Bute] Add Leap Year

---
+++ BasicPrograms/FlipCoin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class FlipCoin
+    {
+        public void HeadsOrTailPercentage()
+        {
+            Random random = new Random();
+            Console.WriteLine("Enter the number of terms");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double k = n;
+            double head = 0D, tail = 0D;
+            while (n > 0)
+            {
+                int checkValue = random.Next(0, 2);
+
+                if (checkValue == 1)
+                {
+                    Console.WriteLine("Heads");
+                    head++;
+
+                }
+                else
+                {
+                    Console.WriteLine("Tails");
+                    tail++;
+                }
+                n--;
+
+            }
+            Console.WriteLine("Heads comes :" + head);
+            Console.WriteLine("Tail comes :" + tail);
+            Console.WriteLine("Heads Percentage : " + ((head / k) * 100));
+            Console.WriteLine("Tailss Percentage : " + ((tail / k) * 100));
+        }
+    }
+}
+
---
+++ BasicPrograms/LeapYear.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_core_programs
+{
+    internal class LeapYear
+    {
+        public void leapYear()
+        {
+            Console.WriteLine("Leap year Program Enter the Year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+            {
+                Console.WriteLine("{0} is a Leap year" + year);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a leap year " + year);
+            }
+        }
+    }
+}
