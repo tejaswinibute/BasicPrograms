e189032ed9eac1b5397376f969b32de02e286fe8
tejaswinibute <tejaswinibute31@gmail.com>
2022-08-15 18:43:01 +0530

    [Tejaswini Bute] Add Even odd

---
+++ BasicPrograms/EvenOrOdd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class EvenOdd
+    {
+        public void EvenOrOdd()
+        {
+            Console.WriteLine("Enter the number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("Entered number is even");
+            }
+            else
+            {
+                Console.WriteLine("Entered number is odd");
+            }
+        }
+    }
+}
