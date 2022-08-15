d5792ad06551bf2afe59c8821a3b4682e3f506ee
tejaswinibute <tejaswinibute31@gmail.com>
2022-08-15 18:29:58 +0530

    [Tejaswini Bute] Add Swap of Two Numbers

---
+++ BasicPrograms/SwapTwoNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class SwapTwoNumber
+    {
+        public void SwapingTwoNumbers()
+        {
+            Console.WriteLine("Enter the first number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("the Value of first number is " + n);
+            Console.WriteLine("the Value of second number is " + m);
+            int c = 0;
+            c = n;
+            n = m;
+            m = c;
+            Console.WriteLine("the Value of first number after swap " + n);
+            Console.WriteLine("the Value of second number after swap " + m);
+
+        }
+    }
+}
