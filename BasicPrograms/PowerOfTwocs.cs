b5259b06522616bf48e160d06899934de15cce63
tejaswinibute <tejaswinibute31@gmail.com>
2022-08-15 18:06:30 +0530

    [Tejaswini Bute] Add Power of Two

---
+++ BasicPrograms/PowerOfTwocs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class PowerOFTwo
+    {
+        public void powOfTwo()
+        {
+            int res = 1;
+            Console.WriteLine("Power of Two Enter the number of terms");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num == 0 || num > 31)
+            {
+                Console.WriteLine("Enter the terms between range 1 to 31");
+            }
+            else
+            {
+                for (int i = 1; i <= num; i++)
+                {
+                    res = res * 2;
+                    Console.WriteLine("Power of 2 raise to" + i + " is " + res);
+                }
+            }
+        }
+    }
+}
--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Basic_core_programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter 1 for Heads and Tails");
+            Console.WriteLine("Enter 2 for Leap Year");
+            Console.WriteLine("Enter 3 for Power of Two");
+            Console.WriteLine("Enter 4 for Harmonic Series");
+            Console.WriteLine("Enter 5 for Prime Factors");
+            Console.WriteLine("Enter 6 for Swapping");
+            Console.WriteLine("Enter 7 for Even or Odd");
+            Console.WriteLine("Enter 8 for Vowels and Consonants");
+            int ans = Convert.ToInt32(Console.ReadLine());
+            switch (ans)
+            { 
+            static void Main(string[] args);
+            EvenOdd Eo = new EvenOdd();
+            Eo.EvenOrOdd();
+            break; 
+        
+                case 8;:
+                    Vowels Vc = new Vowels();
+            Vc.vowelsOrConsonants();
+            break;
+            default;
+                    Console.WriteLine("Entered value is incorrect");
+            break;
+        }
+    }
+}
+}
