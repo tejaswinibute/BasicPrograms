f12907214933482dd2d89591a550444ed536518a
tejaswinibute <tejaswinibute31@gmail.com>
2022-08-15 18:24:10 +0530

    [Tejaswini Bute ] add Prime Factor

---
+++ BasicPrograms/PrimeFactor.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    internal class PrimeFactor
+    {
+
+        public void PrimeFactors()
+        {
+            int c = 2;
+            Console.WriteLine("Enter the number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            while (n > 1)
+            {
+                if (n % c == 0)
+                {
+                    Console.Write(c + " ");
+                    n /= c;
+                }
+                else
+                    c++;
+            }
+        }
+    }
+}
--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -1,34 +1,42 @@
-﻿namespace Basic_core_programs
+﻿namespace BasicPrograms
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Enter 1 for Heads and Tails");
+            Console.WriteLine("Enter 1 for Flip Coin");
             Console.WriteLine("Enter 2 for Leap Year");
             Console.WriteLine("Enter 3 for Power of Two");
             Console.WriteLine("Enter 4 for Harmonic Series");
             Console.WriteLine("Enter 5 for Prime Factors");
-            Console.WriteLine("Enter 6 for Swapping");
-            Console.WriteLine("Enter 7 for Even or Odd");
-            Console.WriteLine("Enter 8 for Vowels and Consonants");
             int ans = Convert.ToInt32(Console.ReadLine());
             switch (ans)
-            { 
-            static void Main(string[] args);
-            EvenOdd Eo = new EvenOdd();
-            Eo.EvenOrOdd();
-            break; 
-        
-                case 8;:
-                    Vowels Vc = new Vowels();
-            Vc.vowelsOrConsonants();
-            break;
-            default;
+            {
+                case 1:
+                    FlipCoin Fc = new FlipCoin();
+                    Fc.HeadsOrTailPercentage();
+                    break;
+                case 2:
+                    LeapYear Ly = new LeapYear();
+                    Ly.leapYear();
+                    break;
+                case 3:
+                    PowerOFTwo Pf = new PowerOFTwo();
+                    Pf.powOfTwo();
+                    break;
+                case 4:
+                    HarmonicNumber Hn = new HarmonicNumber();
+                    Hn.Harmonic();
+                    break;
+                case 5:
+                    Factors primeFactors = new Factors();
+                    primeFactors.PrimeFactors();
+                    break;
+                default:
                     Console.WriteLine("Entered value is incorrect");
-            break;
+                    break;
+            }
         }
     }
-}
 }
