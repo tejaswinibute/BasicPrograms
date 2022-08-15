2abc9a0f2a33f1c2b368bb6fe13da578035af7c3
tejaswinibute <tejaswinibute31@gmail.com>
2022-08-15 18:11:57 +0530

    [Tejaswini Bute] Add Harmonic

---
+++ BasicPrograms/Harmonic.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_core_programs
+{
+    internal class HarmonicNumber
+    {
+        public void Harmonic()
+        {
+            Console.WriteLine("Harmonic number series program");
+            Console.WriteLine("Enter the number of terms: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double i = 1;
+            double sum = 0;
+            while (i <= n)
+            {
+                sum = sum + (1 / i);
+                i++;
+            }
+            Console.WriteLine("The sum of " + i + "th term is=" + sum);
+        }
+    }
+}
