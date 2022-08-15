e958b5fec43bbda2f99870b369ae5cd47cfe2b7a
tejaswinibute <tejaswinibute31@gmail.com>
2022-08-15 18:53:05 +0530

    [Tejaswini Bute] Add Largest Number

---
+++ BasicPrograms/LargestNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    public class LargestNumber
+    {
+        public void large()
+        {
+            float n1, n2, n3;
+            Console.WriteLine("ENTER ANY THREE NUMBERS");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            n2 = Convert.ToInt32(Console.ReadLine());
+            n3 = Convert.ToInt32(Console.ReadLine());
+
+            if (n1 >= n2 && n1 >= n3)
+            {
+                Console.WriteLine("Largest Number is :" + n1);
+            }
+            if (n2 >= n1 && n2 >= n3)
+            {
+                Console.WriteLine("Largest Number is :" + n2);
+            }
+            if (n3 >= n2 && n3 >= n1)
+            {
+                Console.WriteLine("Largest Number is :" + n3);
+            }
+        }
+    }
+}
