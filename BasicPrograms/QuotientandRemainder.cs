3e932c79f420864833b24aa1c5afe455ebefd2ce
tejaswinibute <tejaswinibute31@gmail.com>
2022-08-15 19:05:21 +0530

    [Tejaswini Bute] Add Quotient and Remainder

---
+++ BasicPrograms/QuotientandRemainder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    class QuotientandRemainder
+    {
+        static void Main(string[] args)
+        {
+            int dividend = 50, divisor = 8;
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Dividend:{0} Divisor:{1}", dividend, divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+            Console.ReadLine();
+        }
+    }
+}
